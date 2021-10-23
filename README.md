08c20b60327afd23d2034280065ab07bbabf290e
rpavanello97 <rpavanello97@hotmail.com>
2021-10-22 21:00:06 -0300

    Putting the field validation in a service.

--- README.md
+++ README.md
@@ -25,4 +25,5 @@ Commands
     Working with json-server
     1) npm install -g json-server
     2) json-server --watch db.json
+
    
--- src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
+++ src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidateErrorsService } from 'src/app/shared/components/fields/validate-fields.service';
 
 @Component({
   selector: 'dio-cadastro-filmes',
@@ -11,7 +12,9 @@ export class CadastroFilmesComponent implements OnInit {
 
   registration: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, 
+              public validation: ValidateErrorsService
+              ) { }
 
   get f() {
     return this.registration.controls;
@@ -31,14 +34,12 @@ export class CadastroFilmesComponent implements OnInit {
   }
 
   save(): void {
-    
     this.registration.markAllAsTouched();
 
     if (this.registration.invalid) {
       return
     }
-
-    alert('Sucesso \n\n'+ JSON.stringify(this.registration.value, null, 4));
+    alert('Sucesso \n\n' + JSON.stringify(this.registration.value, null, 4));
   }
 
   restartForm(): void {
